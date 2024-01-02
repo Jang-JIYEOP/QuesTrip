ec3c89cf4db3be3497f0384d8e206ae2680286e2
DESKTOP-8UFR6LN\user1 <jangjiyeop24@gmail.com>
2024-01-02 16:00:22 +0900

    게시판 기능 구현

---
+++ QuesTrip/src/main/java/com/kh/questrip/board/vo/BoardVo.java
@@ -0,0 +1,12 @@
+package com.kh.questrip.board.vo;
+
+import lombok.Data;
+
+@Data
+public class BoardVo {
+	private String no;
+	private String memberNo;
+	private String title;
+	private String content;
+	private String delYn;
+}
