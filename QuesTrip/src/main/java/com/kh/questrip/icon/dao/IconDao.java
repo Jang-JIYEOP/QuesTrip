3e874b79ac370d3c08eb28b79d83a3bae676a1da
DESKTOP-8AVTDS3\user1 <qkrqjafuf2@gmail.com>
2024-01-15 17:35:48 +0900

    [아이콘 구매, 대표 아이콘 설정 성공!]

--- QuesTrip/src/main/java/com/kh/questrip/icon/dao/IconDao.java
+++ QuesTrip/src/main/java/com/kh/questrip/icon/dao/IconDao.java
@@ -1,21 +1,48 @@
 package com.kh.questrip.icon.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.questrip.icon.vo.IconVo;
+import com.kh.questrip.icon.vo.buyerVo;
 import com.kh.questrip.quest.vo.SearchInfoVo;
 
 @Repository
 public class IconDao {
 
 	public List<IconVo> list(SqlSessionTemplate sst, SearchInfoVo vo) {
-		return sst.selectList("IconMapper.list", vo);
+		return sst.selectList("IconMapper.shopList", vo);
 	}
 	
 	public List<IconVo> pageList(SqlSessionTemplate sst, SearchInfoVo vo) {
-		return sst.selectList("IconMapper.pageList", vo);
+		return sst.selectList("IconMapper.shopPageList", vo);
+	}
+
+	public List<IconVo> myIconAll(SqlSessionTemplate sst, SearchInfoVo vo) {
+		return sst.selectList("IconMapper.myIconAll", vo);
+	}
+
+	public List<IconVo> pageMyIcon(SqlSessionTemplate sst, SearchInfoVo vo) {
+		
+		return sst.selectList("IconMapper.pageMyIcon",vo);
+	}
+
+	public int update(SqlSessionTemplate sst, buyerVo vo) {
+		return sst.update("IconMapper.update",vo);
+	}
+
+	public int memberPointUpdate(buyerVo vo, SqlSessionTemplate sst) {
+		return sst.update("IconMapper.memberPointUpdate",vo);
+	}
+
+	public int pointInsert(buyerVo vo, SqlSessionTemplate sst) {
+		return sst.insert("IconMapper.pointInsert",vo);
+	}
+
+	public int memberIconInsert(buyerVo vo, SqlSessionTemplate sst) {
+		return sst.insert("IconMapper.memberIconInsert",vo);
 	}
 }
